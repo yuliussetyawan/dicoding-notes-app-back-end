25e6a00be60e984bc42e34d8408159d7aae51e3c
yuliussetyawan <yuliusetyawan@gmail.com>
2022-11-16 00:14:24 +0700

    chapter 1 menyimpan catatan

---
+++ src/handler.js
@@ -0,0 +1,51 @@
+// npm install nanoid@3.x.x
+const { nanoid } = require('nanoid')
+const notes = require('./notes')
+
+const addNoteHandler = (request, h) => {
+  // mendapatkan body request di Hapi
+  const { title, tags, body } = request.payloads
+  // Properti ID
+  const id = nanoid(16)
+  // createdAt & UpdatedAt ID
+  const createdAt = new Date().toISOString()
+  const UpdatedAt = createdAt
+
+  const newNote = {
+    title,
+    tags,
+    body,
+    id,
+    createdAt,
+    UpdatedAt
+  }
+
+  notes.push(newNote)
+
+  // menentukan apakah newNote sudah masuk ke dalam array notes?
+  const isSuccess =
+    notes.filter(note => {
+      note.id === id
+    }).length > 0
+
+  if (isSuccess) {
+    const response = h.response({
+      status: 'success',
+      message: 'Data berhasil ditambahkan',
+      data: {
+        noteId: id
+      }
+    });
+    response.code(201);
+    return response;
+  }
+  const response = h.response({
+    status: "fail",
+    message: "catatan gagal ditambahkan",
+  });
+  response.code(500);
+  return response;
+}
+
+// Objek literals bertujuan untuk memudahkan ekspor lebih dari satu nilai pada satu berkas JavaScript.
+module.exports = { addNoteHandler };
