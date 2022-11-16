a071d6af26ee286f4c05e53c567a6644a99e3020
yuliussetyawan <yuliusetyawan@gmail.com>
2022-11-16 13:46:15 +0700

    chapter 2 menampilkan catatan

--- src/handler.js
+++ src/handler.js
@@ -23,26 +23,61 @@ const addNoteHandler = (request, h) => {
   notes.push(newNote)
 
   // menentukan apakah newNote sudah masuk ke dalam array notes?
-  const isSuccess = notes.filter((note) => note.id === id).length > 0;
+  const isSuccess = notes.filter(note => note.id === id).length > 0
 
   if (isSuccess) {
     const response = h.response({
       status: 'success',
       message: 'Catatan berhasil ditambahkan',
       data: {
-        noteId: id,
-      },
-    });
-    response.code(201);
-    return response;
+        noteId: id
+      }
+    })
+    response.code(201)
+    return response
   }
   const response = h.response({
     status: 'fail',
     message: 'catatan gagal ditambahkan'
-  });
-  response.code(500);
-  return response;
+  })
+  response.code(500)
+  return response
 }
 
+// menampilkan notes di halaman home. parameter h, req tidak digunakan
+const getAllNotesHandler = () => ({
+  status: 'success',
+  data: {
+    notes
+  },
+});
+
+// Menampilkan hasil notes setelah diklik
+const getNoteByIdHandler = (request, h) =>{
+  const {id} = request.params;
+  // dapatkan objek note dengan id tersebut dari objek array notes
+  const note = notes.filter((n)=> n.id === id)[0];
+  console.log(note);
+
+  if (note !== undefined){
+    return{
+      status: "success",
+      data:{
+        note,
+      },
+    }
+  };
+
+  const response = h.response({
+    status: "fail",
+    message: "catatan tidak ditemukan",
+
+  });
+  response.code = 404;
+  return response;
+
+}
+
+
 // Objek literals bertujuan untuk memudahkan ekspor lebih dari satu nilai pada satu berkas JavaScript.
-module.exports = { addNoteHandler }
+module.exports = { addNoteHandler, getAllNotesHandler, getNoteByIdHandler};
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-const { addNoteHandler } = require("./handler");
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler } = require("./handler");
 
 const routes = [
     {
@@ -6,6 +6,19 @@ const routes = [
         path: "/notes",
         handler: addNoteHandler,
     },
+    // menampilkan notes di home
+    {
+        method: "GET",
+        path: "/notes",
+        handler: getAllNotesHandler,
+
+    },
+    // menampilkan notes setelah diklik
+    {
+        method: "GET",
+        path: `/notes/{id}`,
+        handler: getNoteByIdHandler,
+    }
 ];
 
 module.exports = routes;
